efa40b78b9145332c78bcd3144f62c4c583058f1
Marie Evanno <45174964+Stellarya@users.noreply.github.com>
2026-05-12 16:16:43 +0200

    feat(typeTest) : add entity

---
+++ src/Entity/TypeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TableReferenceTrait;
+use App\Repository\TypeTestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TypeTestRepository::class)]
+class TypeTest
+{
+
+    use TableReferenceTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy:'IDENTITY')]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+}
