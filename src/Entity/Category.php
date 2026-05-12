83d4f2b3a4721ff789f095e6ba563dfb0285510f
Marie Evanno <45174964+Stellarya@users.noreply.github.com>
2026-05-12 15:03:08 +0200

    feat(level) : add entity

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Entity\Traits\TableReferenceTrait;
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -19,6 +21,17 @@ class Category
     #[ORM\Column(length: 255)]
     private ?string $libelle = null;
 
+    /**
+     * @var Collection<int, Level>
+     */
+    #[ORM\OneToMany(targetEntity: Level::class, mappedBy: 'category')]
+    private Collection $levels;
+
+    public function __construct()
+    {
+        $this->levels = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -32,6 +45,36 @@ class Category
     public function setLibelle(string $libelle): static
     {
         $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Level>
+     */
+    public function getLevels(): Collection
+    {
+        return $this->levels;
+    }
+
+    public function addLevel(Level $level): static
+    {
+        if (!$this->levels->contains($level)) {
+            $this->levels->add($level);
+            $level->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLevel(Level $level): static
+    {
+        if ($this->levels->removeElement($level)) {
+            // set the owning side to null (unless already changed)
+            if ($level->getCategory() === $this) {
+                $level->setCategory(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Level.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TableReferenceTrait;
+use App\Repository\LevelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LevelRepository::class)]
+class Level
+{
+    use TableReferenceTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy:'IDENTITY')]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\ManyToOne(inversedBy: 'levels')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
