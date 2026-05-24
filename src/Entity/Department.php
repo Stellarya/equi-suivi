b692b967cec395db9bfa52e820a81b01b624c107
Marie Evanno <45174964+Stellarya@users.noreply.github.com>
2026-05-24 18:12:40 +0200

    feat(department) : add entity

---
+++ src/Entity/Department.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TableReferenceTrait;
+use App\Repository\DepartmentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DepartmentRepository::class)]
+class Department
+{
+    
+    use TableReferenceTrait;
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy:'IDENTITY')]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $numberDepartment = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumberDepartment(): ?int
+    {
+        return $this->numberDepartment;
+    }
+
+    public function setNumberDepartment(int $numberDepartment): static
+    {
+        $this->numberDepartment = $numberDepartment;
+
+        return $this;
+    }
+}
